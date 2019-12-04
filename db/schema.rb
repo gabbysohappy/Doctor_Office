6d8cef74b7179ff047290e22e256b0f3cee6d0df
Aaron Cookson <cookson.aaron@gmail.com>
2019-12-03 18:36:07 -0700

    added appointments

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 2019_12_03_203302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "appointmen", force: :cascade do |t|
-    t.datetime "time"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "appointments", force: :cascade do |t|
     t.datetime "time"
     t.datetime "created_at", precision: 6, null: false
